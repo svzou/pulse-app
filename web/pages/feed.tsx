93b8f94a5a3144956993a419b8d5c2043da69b3a
Sophia Zou <111460074+svzou@users.noreply.github.com>
2025-04-20 23:34:13 -0400

    done

--- web/pages/feed.tsx
+++ web/pages/feed.tsx
@@ -1,47 +1,47 @@
-/**
- * Component to display an infinitely scrolling feed of posts.
- *
- * @author Ajay Gandecha <agandecha@unc.edu>
- * @license MIT
- * @see https://comp426-25s.github.io/
- */
+// /**
+//  * Component to display an infinitely scrolling feed of posts.
+//  *
+//  * @author Ajay Gandecha <agandecha@unc.edu>
+//  * @license MIT
+//  * @see https://comp426-25s.github.io/
+//  */
 
-import { Fragment } from "react";
-import { InView } from "react-intersection-observer";
-import { z } from "zod";
-import { Post } from "@/utils/supabase/models/post";
-import { InfiniteData } from "@tanstack/react-query";
-import PostCard from "./post";
-import { Separator } from "./ui/separator";
-import { User } from "@supabase/supabase-js";
+// import { Fragment } from "react";
+// import { InView } from "react-intersection-observer";
+// import { z } from "zod";
+// import { Post } from "@/utils/supabase/models/post";
+// import { InfiniteData } from "@tanstack/react-query";
+// import PostCard from "./post";
+// import { Separator } from "./ui/separator";
+// import { User } from "@supabase/supabase-js";
 
-type PostFeedProps = {
-  user: User;
-  posts: InfiniteData<z.infer<typeof Post>[]> | undefined;
-  fetchNext: () => void;
-};
-export default function PostFeed({ user, posts, fetchNext }: PostFeedProps) {
-  return (
-    <div className="divide-y divide-gray-200 dark:divide-gray-700">
-      {/* Posts are grouped by pages, so we need to iterate first over page, then posts. */}
-      {posts &&
-        posts.pages.map((page) => {
-          return page.map((post, postIndex) => (
-            <Fragment key={`post_${post.id}`}>
-              {/* This in-view component triggers an action whenever it appears on the
-              screen. Since the page length is 25, this item is placed on the 20th element
-              per page. Once the user scrolls to near the end of the page and this element
-              appears, new data is loaded (infinite scrolling) */}
-              {postIndex === 20 && (
-                <InView onChange={(inView) => inView && fetchNext()}></InView>
-              )}
-              <div className="p-4 flex flex-col gap-2">
-              <PostCard user={user} post={post} />
-              </div>
-              <Separator className="bg-gray-200 dark:bg-gray-700" />
-            </Fragment>
-          ));
-        })}
-    </div>
-  );
-}
+// type PostFeedProps = {
+//   user: User;
+//   posts: InfiniteData<z.infer<typeof Post>[]> | undefined;
+//   fetchNext: () => void;
+// };
+// export default function PostFeed({ user, posts, fetchNext }: PostFeedProps) {
+//   return (
+//     <div className="divide-y divide-gray-200 dark:divide-gray-700">
+//       {/* Posts are grouped by pages, so we need to iterate first over page, then posts. */}
+//       {posts &&
+//         posts.pages.map((page) => {
+//           return page.map((post, postIndex) => (
+//             <Fragment key={`post_${post.id}`}>
+//               {/* This in-view component triggers an action whenever it appears on the
+//               screen. Since the page length is 25, this item is placed on the 20th element
+//               per page. Once the user scrolls to near the end of the page and this element
+//               appears, new data is loaded (infinite scrolling) */}
+//               {postIndex === 20 && (
+//                 <InView onChange={(inView) => inView && fetchNext()}></InView>
+//               )}
+//               <div className="p-4 flex flex-col gap-2">
+//               <PostCard user={user} post={post} />
+//               </div>
+//               <Separator className="bg-gray-200 dark:bg-gray-700" />
+//             </Fragment>
+//           ));
+//         })}
+//     </div>
+//   );
+// }
